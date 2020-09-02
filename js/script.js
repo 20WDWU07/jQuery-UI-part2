79cf964538451a6b588c25647f3f96232ec58343
beulathamban <beulathamban@gmail.com>
2020-09-02 14:33:53 +1200

    devices

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 console.log('js');
-  $('#loginSection').hide();
+  $('#loginSection, #controlSection, #myDevices').hide();
+
 
 $(document).ready(function(){
   $('#login').click(function(){
     $('#loginSection').show();
-    $('nav').hide();
+    $('.top-nav').hide();
   });
   $('#username').click(function(){
     // alert($(this).val());
     document.getElementById('username').value="";
+  });
+  $('#loginBtn').click(function(){
+    $('#controlSection').show();
+
+      $('#loginSection').hide();
+    // $('nav').hide();
+  });
+  $('#controlDevice').click(function(){
+    $('#myDevices').show();
+    $('#controlSection').hide();
+    $('#loginSection').hide();
+    // $('nav').hide();
   });
 });
