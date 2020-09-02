1a3a08386ae24c4f0f140a50cf164f0c73539bb2
Beula Samuel <beulasamuel@Beulas-MacBook-Pro.local>
2020-09-02 22:22:42 +1200

    device controls

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 console.log('js');
-  $('#loginSection, #controlSection, #myDevices').hide();
+  $('#loginSection, #controlSection, #myDevices, #switcheOff').hide();
 
 
 $(document).ready(function(){
@@ -25,10 +25,57 @@ $(document).ready(function(){
   });
   $('#switcheOn').click(function(){
     $('#house').css('color', 'yellow');
+    $('#switcheOff').show();
+      $('#switcheOn').hide();
 
   });
   $('#switcheOff').click(function(){
-    $('#house').css('color', 'black');     
+    $('#house').css('color', 'black');
+    $('#switcheOff').hide();
+    $('#switcheOn').show();
 
   });
+  $('#lights').click(function(){
+
+    $('#tvControl').hide();
+    $('#lightControl').show();
+
+  });
+
+  // slider for light intensity
+  $( function() {
+    var select = $( "#minbeds" );
+    var slider = $( "<div id='slider'></div>" ).insertAfter( select ).slider({
+      min: 1,
+      max: 6,
+      range: "min",
+      value: select[ 0 ].selectedIndex + 1,
+      slide: function( event, ui ) {
+        select[ 0 ].selectedIndex = ui.value - 1;
+      }
+    });
+    $( "#minbeds" ).on( "change", function() {
+      slider.slider( "value", this.selectedIndex + 1 );
+    });
+  } );
+
+    // tabs for devices
+  $( function() {
+    $( "#tabs" ).tabs();
+  } );
+
+  // slider for tv volume
+  $( function() {
+    $( "#slider-vertical" ).slider({
+      orientation: "vertical",
+      range: "min",
+      min: 0,
+      max: 100,
+      value: 60,
+      slide: function( event, ui ) {
+        $( "#amount" ).val( ui.value );
+      }
+    });
+    $( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
+  } );
 });
